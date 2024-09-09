cd8aa63b770e28f552e0a25bf2c2fd5b2447b83c
Velas <pvelaalm@myuax.com>
2024-09-09 14:09:39 +0200

    Creación proyecto

---
+++ src/test/java/com/example/starkdev/StarkDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starkdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarkDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
